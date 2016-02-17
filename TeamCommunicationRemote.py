6a98cd87591bd8386ba5102e27eb616f55cff46b
Dimitri DUBOIS <dimitri.dubois@umontpellier.fr>
2016-02-17 16:38:29 +0100

    Add code files

---
+++ TeamCommunicationRemote.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import random
+from twisted.internet import defer
+from client.cltremote import IRemote
+import TeamCommunicationParams as pms
+import TeamCommunicationTexts as texts_TC
+from TeamCommunicationGui import GuiDecision, DAdditionnalquestions, \
+    DQuestionDictator
+
+
+logger = logging.getLogger("le2m")
+
+
+class RemoteTC(IRemote):
+    """
+    Class remote, remote_ methods can be called by the server
+    """
+    def __init__(self, le2mclt):
+        IRemote.__init__(self, le2mclt)
+        self._le2mclt = le2mclt
+        self._tcremoteplayer = None  # to send message
+        self._ecran_decision = None
+        self._payoffs = {}
+
+    @property
+    def tcremoteplayer(self):
+        return self._tcremoteplayer
+
+    def remote_configure(self, tcremoteplayer, params):
+        logger.info(u"{} configure".format(self._le2mclt.uid))
+        self._tcremoteplayer = tcremoteplayer
+        for k, v in params.viewitems():
+            setattr(pms, k, v)
+
+    def remote_newperiod(self, period):
+        logger.info(u"{} Period {}".format(self._le2mclt.uid, period))
+        self.currentperiod = period
+        if self.currentperiod == 1:
+            del self.histo[1:]
+
+    def remote_display_decision(self):
+        """
+        Display the decision screen
+        :return: deferred
+        """
+        logger.info(u"{} Decision".format(self._le2mclt.uid))
+        defered = defer.Deferred()
+        if self._le2mclt.simulation:
+            self._ecran_decision = GuiDecision(
+                defered, True, self._le2mclt.screen, self)
+            return defered
+        else:
+            self._ecran_decision = GuiDecision(
+                defered, self._le2mclt.automatique, self._le2mclt.screen, self)
+            self._ecran_decision.show()
+            return defered
+
+    @defer.inlineCallbacks
+    def send_look(self, grille):
+        logger.info(u"{} send_look grille {}".format(self._le2mclt.uid, grille))
+        yield (self.tcremoteplayer.callRemote("send_look", grille))
+
+    @defer.inlineCallbacks
+    def send_try(self, grille, valeur):
+        logger.info(u"{} send_try grille {} valeur {}".format(
+            self._le2mclt.uid, grille, valeur))
+        yield (self.tcremoteplayer.callRemote("send_try", grille, valeur))
+
+    @defer.inlineCallbacks
+    def send_message(self, message):
+        logger.info(u"{} send_message {}".format(self._le2mclt.uid, message))
+        yield (self.tcremoteplayer.callRemote("send_message", message))
+
+    def remote_display_message(self, message):
+        logger.info(u"{} display_message {}".format(self._le2mclt.uid, message))
+        if self._le2mclt.simulation:
+            pass
+        else:
+            self._ecran_decision.add_message(message)
+
+    def remote_display_additionnalquestions(self, nbanswers):
+        logger.info(u"{} display_additionnalquestions".format(
+            self._le2mclt.uid))
+        if self._le2mclt.simulation:
+            rep = {"TC_confidence": random.randint(0, nbanswers),
+                   "TC_jobsatisfaction": random.randint(0, 7)}
+            if pms.TREATMENT == pms.get_treatement("avec_communication"):
+                rep["TC_infosatisfaction"] = random.randint(0, 7)
+            logger.info(u"{} renvoi {}".format(self._le2mclt.uid, rep))
+            return rep
+        else:
+            defered = defer.Deferred()
+            ecran_additionnalquestions = DAdditionnalquestions(
+                defered, self._le2mclt.automatique, self._le2mclt.screen,
+                nbanswers)
+            ecran_additionnalquestions.show()
+            return defered
+
+    def remote_display_questionapresdictator(self):
+        logger.info(u"{} remote_display_questionapresdictator".format(
+            self._le2mclt.uid))
+        if self._le2mclt.simulation:
+            rep = random.randint(0, 10)
+            logger.info(u"{} renvoi {}".format(self._le2mclt.uid, rep))
+            return rep
+        else:
+            defered = defer.Deferred()
+            screen = DQuestionDictator(
+                defered, self._le2mclt.automatique, self._le2mclt.screen)
+            screen.show()
+            return defered
+
+    def remote_display_summary(self, period_content):
+        txt = texts_TC.get_text_recapitulatif(period_content)
+        return self._le2mclt.get_remote("base").remote_display_information(txt)
+
+    def remote_set_payoffs_TC(self, sequence, in_euros, in_ecus=None):
+        self.remote_set_payoffs(in_euros, in_ecus)
+        self._payoffs[sequence] = {
+            "euro": self.payoff_euros, "ecu": self.payoff_ecus,
+            "txt": self.payoff_text}
+
+    def remote_display_payoffs_TC(self, sequence):
+        return self.le2mclt.get_remote("base").remote_display_information(
+            self._payoffs[sequence]["txt"])
---
+++ TeamCommunicationTexts.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Ce module contient les textes des écrans
+"""
+__author__ = "Dimitri DUBOIS"
+
+
+from collections import namedtuple
+from util.utiltools import get_pluriel
+import TeamCommunicationParams as pms
+
+import os
+import configuration.configparam as params
+import gettext
+localedir = os.path.join(params.getp("PARTSDIR"), "TeamCommunication", "locale")
+trans_TC = gettext.translation(
+  "TeamCommunication", localedir, languages=[params.getp("LANG")]).ugettext
+
+
+TITLE_MSG = namedtuple("TITLE_MSG", "titre message")
+
+
+def get_text_explanation():
+    txt = trans_TC(u"To display a grid, click on its number.")
+    txt += trans_TC(u"\nTo indicate the number of '1' included in the grid, enter "
+               u"this number directly in the box under its number")
+    if pms.TREATMENT == pms.get_treatment("avec_communication"):
+        txt += trans_TC(u"The chat window allows you to communicate freely for {} "
+                   u"minutes with other group players.".format(
+                        pms.TEMPS_PARTIE.minute))
+
+
+# ECRAN RECAPITULATIF ==========================================================
+def get_text_recapitulatif(period_content):
+    txt = trans_TC(u"You've found {}.<br />Your group have found a total of "
+              u"{}.<br />Each group member earns {}.").format(
+        get_pluriel(period_content.get("TC_goodanswers"),
+                    trans_TC(u"good answer")),
+        get_pluriel(period_content.get("TC_goodanswers_group"),
+                    trans_TC(u"good answer")),
+        get_pluriel(period_content.get("TC_periodpayoff"), u"ecu"))
+    return txt
+
+
+# ADDITIONNAL QUESTIONS
+def get_text_reponses(nbanswers):
+    txt = trans_TC(u"Among your {}, how much to you think are rights?").format(
+        get_pluriel(nbanswers, trans_TC(u"answer")))
+    return txt
+
+
+def get_text_infosatisfaction():
+    txt = trans_TC(u"In a scale ranged from 1 (not satisfied at all) to 7 "
+                   u"(very satisfied), <br />where is your level of "
+                   u"satisfaction in regards of <br />the informations you "
+                   u"exchanged with your group members?")
+    return txt
+
+
+def get_text_jobsatisfaction():
+    txt = trans_TC(u"In a scale ranged from 1 (not satisfied at all) to 7 "
+                   u"(very satisfied), <br />where is you level of "
+                   u"satisfaction in regards of the task <br />you "
+                   u"participated in?")
+    return txt
+
+
+def get_text_predictiondictator():
+    txt = trans_TC(u"To your opinion, how much was sent, on average, to "
+                   u"players B in the current session?")
+    return txt
